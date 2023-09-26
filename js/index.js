6caefce3b78e5f7d7b91131fdc64a08a0ee0729a
Angger Nur <amggern514@gmail.com>
2023-09-26 14:15:11 +0700

    refactoring code css - html - js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ let todos = [];
 let searchResults = [];
 const checkbox = document.getElementById("inputBookIsComplete");
 const confirmDelete = document.querySelector(".wrapper-confirm-delete");
+const confirmDeleteAll = document.querySelector(".wrapper-confirm-delete-all");
 const keteranganBuku = document.getElementById("keteranganBuku");
+const buttonClose = document.querySelector(".close-confirm-delete");
+const buttonCloseAll = document.querySelector(".close-confirm-delete-all");
 const deleteBook = document.getElementById("deleteBook");
 const batalDelete = document.getElementById("batalDelete");
 const deleteAll = document.querySelector(".removeAllBooks");
@@ -32,11 +35,17 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   deleteAll.addEventListener("click", () => {
-    console.log("hapus");
-    todos = [];
-    localStorage.clear();
-    renderResults();
-    document.dispatchEvent(new Event(RENDER_EVENT));
+    showDeleteAllConfirmation();
+  });
+
+  // event untuk button close confirmation delete
+  buttonClose.addEventListener("click", () => {
+    confirmDelete.classList.remove("show-confirm-delete");
+  });
+
+  // event untuk button close confirmation delete All
+  buttonCloseAll.addEventListener("click", () => {
+    confirmDeleteAll.classList.remove("show-confirm-delete-all");
   });
 
   // function untuk mengambil input user
@@ -55,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (deleteBook) {
       todos = todos.filter((item) => item.id !== id);
       searchResults = searchResults.filter((item) => item.id !== id);
-      localStorage.setItem("todos", JSON.stringify(todos));
+      saveTodos();
       renderResults();
     }
     document.dispatchEvent(new Event(RENDER_EVENT));
@@ -96,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
       isComplete: checkbox,
     };
     todos = [...todos, todo];
-    localStorage.setItem("todos", JSON.stringify(todos));
+    saveTodos();
     document.getElementById("inputBookTitle").value = "";
     document.getElementById("inputBookAuthor").value = "";
     document.getElementById("inputBookYear").value = "";
@@ -124,26 +133,28 @@ document.addEventListener("DOMContentLoaded", () => {
     divWrapperButtons.classList.add("action");
 
     const buttonCheklist = document.createElement("button");
-    buttonCheklist.textContent = "";
     buttonCheklist.classList.add("green");
     const iconUndo = document.createElement("i");
     iconUndo.classList.add("fa-solid", "fa-arrow-rotate-left");
-    buttonCheklist.appendChild(iconUndo);
+    const ketButtonUndo = document.createElement("span");
 
     const buttonDelete = document.createElement("button");
-    buttonDelete.textContent = "Hapus buku";
     buttonDelete.classList.add("red");
     const iconDelete = document.createElement("i");
+    const ketButtonDelete = document.createElement("span");
     iconDelete.classList.add("fas", "fa-trash");
-    buttonCheklist.appendChild(iconDelete);
+    ketButtonDelete.textContent = "Hapus buku";
+    buttonDelete.append(iconDelete, ketButtonDelete);
 
     if (todo.isComplete) {
-      buttonCheklist.textContent = "Belum Selesai dibaca";
+      ketButtonUndo.textContent = "Belum Selesai dibaca";
+      buttonCheklist.append(iconUndo, ketButtonUndo);
       buttonCheklist.addEventListener("click", () => {
         undoFromTodoComplete(todo.id);
       });
     } else {
-      buttonCheklist.textContent = "Selesai dibaca";
+      ketButtonUndo.textContent = "Selesai dibaca";
+      buttonCheklist.append(iconUndo, ketButtonUndo);
       buttonCheklist.addEventListener("click", () => {
         undoFromTodoUnComplete(todo.id);
       });
@@ -157,8 +168,26 @@ document.addEventListener("DOMContentLoaded", () => {
     articleElement.appendChild(divWrapperButtons);
 
     articleElement.setAttribute("id", `todo-${todo.id}`);
-
     return articleElement;
+  }
+  // function untuk menghapus semua todo dari todos by Id
+  function showDeleteAllConfirmation() {
+    confirmDeleteAll.classList.add("show-confirm-delete-all");
+
+    const deleteAllBook = document.getElementById("deleteAllBook");
+    deleteAllBook.addEventListener("click", () => {
+      todos = [];
+      localStorage.clear();
+      confirmDeleteAll.classList.remove("show-confirm-delete-all");
+      showButtonDeleteAll();
+      renderResults();
+      document.dispatchEvent(new Event(RENDER_EVENT));
+    });
+
+    const batalDeleteAllBook = document.getElementById("batalDeleteAllBook");
+    batalDeleteAllBook.addEventListener("click", () => {
+      confirmDeleteAll.classList.remove("show-confirm-delete-all");
+    });
   }
 
   // function untuk menghapus todo dari todos by Id
@@ -196,28 +225,38 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-  // function untuk save data ke local storange
-  const storedTodos = localStorage.getItem("todos");
-
-  if (storedTodos) {
-    todos = JSON.parse(storedTodos);
-    console.log(todos);
-    renderResults();
-    document.dispatchEvent(new Event(RENDER_EVENT));
-  }
-
-  submitForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    addTodo();
-  });
-  // custom event sebagai patokan jika ada perubahan pada variable todos
-  document.addEventListener(RENDER_EVENT, () => {
+  // function toggle button delete all
+  function showButtonDeleteAll() {
     if (todos.length === 0) {
       deleteAll.style.display = "none";
     } else {
       deleteAll.style.display = "block";
     }
+  }
+
+  // function untuk menyimpan todo ke local-storange
+  function saveTodos() {
     localStorage.setItem("todos", JSON.stringify(todos));
+  }
+  // even submit add todo
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTodo();
+  });
+
+  const storedTodos = localStorage.getItem("todos");
+  if (storedTodos) {
+    todos = JSON.parse(storedTodos);
+    renderResults();
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+  showButtonDeleteAll();
+
+  // custom event sebagai patokan jika ada perubahan pada variable todos
+
+  document.addEventListener(RENDER_EVENT, () => {
+    showButtonDeleteAll();
+    saveTodos();
     renderResults();
   });
 });
